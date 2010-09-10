bed285401563abbab7c2061effd631d38c94964d
jemian <jemian@3cb32c87-4c34-0410-8c40-b60f3c13e47e>
2010-09-10 06:01:21 +0000

    refactor XML support to use ElementTree (lost use of XPath) BUT now ignores name of root element - Is this GOOD?

--- USAXS/scanLog/scanlog.py
+++ USAXS/scanLog/scanlog.py
@@ -15,129 +15,75 @@ Record starting and ending times of USAXS scans
 #**************************************************************************
 
 import sys
-import time
-# TODO refactor XML support using ElementTree
 from xml.etree import ElementTree
-# SEE https://subversion.xor.aps.anl.gov/trac/bcdaext/browser/pvrrd/xmlSupport.py
-import xml.dom.minidom
-#import xml.dom.ext
-#import xml.xpath
+import xmlSupport
 
 #**************************************************************************
 
 def usage():
-   print 'usage:  scanlog.py started|ended number fileName "the title"'
-
-def appendTextNode(doc, scanEntry, tag, value):
-    # TODO refactor using ElementTree
-    elem = doc.createElement(tag)
-    scanEntry.appendChild(elem)
-    node = doc.createTextNode(value)
-    elem.appendChild(node)
-
-def appendDateTimeNode(doc, scanEntry, tag):
-    # TODO refactor using ElementTree
-    elem = doc.createElement(tag)
-    scanEntry.appendChild(elem)
-    elem.setAttribute('date', xmlDate())
-    elem.setAttribute('time', xmlTime())
-
-def xmlDate():
-    str = time.strftime('%Y-%m-%d')    # XML date format
-    return str
-
-def xmlTime():
-    str = time.strftime('%H:%M:%S')    # XML time format
-    return str
+    print 'usage:  scanlog.py started|ended number fileName "the title"'
 
 def buildID(number, fileName):
     return "%s:%s" % (number, fileName)
 
-def locateScanID(doc, scanID):
-    query = '//scan[@id="%s"]' % scanID
-    return xml.xpath.Evaluate(query, doc)
-
 def startScanEntry(scanLogFile, number, fileName, title):
     scanID = buildID(number, fileName)
-    doc = openScanLogFile(scanLogFile)
-    result = locateScanID(doc, scanID)
-    if len(result)>0:
+    doc = xmlSupport.openScanLogFile(scanLogFile)
+    scanNode = xmlSupport.locateScanID(doc, scanID)
+    if len(scanNode)>0:
         print "ERROR: One or more scans matches in the log file."
-	return
+        return
+    #---
+    root = doc.getroot()
+    scanEntry = ElementTree.Element('scan')
+    scanEntry.set('number', number)
+    scanEntry.set('state', 'scanning')
+    scanEntry.set('id', scanID)
     # put this scan at the end of the list
-    # TODO refactor using ElementTree
-    scanEntry = doc.createElement('scan')
-    lastScanEntry = doc.childNodes[len(doc.childNodes)-1]
-    lastScanEntry.appendChild(scanEntry)
+    root.append(scanEntry)
     #---
-    # TODO refactor using ElementTree
-    scanEntry.setAttribute('number', number)
-    scanEntry.setAttribute('state', 'scanning')
-    scanEntry.setAttribute('id', scanID)
+    xmlSupport.appendTextNode(doc, scanEntry, 'title', title)
+    xmlSupport.appendTextNode(doc, scanEntry, 'file', fileName)
+    xmlSupport.appendDateTimeNode(doc, scanEntry, 'started')
     #---
-    appendTextNode(doc, scanEntry, 'title', title)
-    appendTextNode(doc, scanEntry, 'file', fileName)
+    xmlSupport.flagRunawayScansAsUnknown(doc, scanID)
     #---
-    appendDateTimeNode(doc, scanEntry, 'started')
-    #---
-    # now look for any other scans with @state='scanning' and change state to 'unknown'
-    # TODO refactor using ElementTree
-    for e in xml.xpath.Evaluate('//scan[@state="scanning"]', doc):
-        if (e.getAttribute('id') != scanID):    # don't change the new node
-	    e.setAttribute('state', 'unknown')  # but THIS node is not scanning anymore
-    #---
-    writeScanLogFile(scanLogFile, doc)
-    return
+    xmlSupport.writeScanLogFile(scanLogFile, doc)
+
 
 def endScanEntry(scanLogFile, number, fileName):
     # find scan based on ID (above)
     scanID = buildID(number, fileName)
-    doc = openScanLogFile(scanLogFile)
-    result = locateScanID(doc, scanID)
-    if len(result)==0:
+    doc = xmlSupport.openScanLogFile(scanLogFile)
+    scanNode = xmlSupport.locateScanID(doc, scanID)
+    if len(scanNode)==None:
         print "ERROR: Could not find that scan in the log file."
-	return
-    elif len(result)>1:
-        print "ERROR: More than one scan matches in the log file! (Should not happen)"
-	return
-    scanEntry = result[0]
-    # TODO refactor using ElementTree
-    if (scanEntry.getAttribute('state')=='scanning'):
-        scanEntry.setAttribute('state', 'complete')   # set scan/@state="complete"
-        appendDateTimeNode(doc, scanEntry, 'ended')
-        writeScanLogFile(scanLogFile, doc)
-    return
+        return
+    #---
+    if (scanNode.get('state')=='scanning'):
+        scanNode.set('state', 'complete')   # set scan/@state="complete"
+        xmlSupport.appendDateTimeNode(doc, scanNode, 'ended')
+        xmlSupport.writeScanLogFile(scanLogFile, doc)
 
-def openScanLogFile(xmlFile):
-    # TODO refactor using ElementTree
-    scanLog = xml.dom.minidom.parse(xmlFile)
-    return scanLog
-
-def writeScanLogFile(xmlFile, scanLog):
-    f = open(xmlFile, 'w')
-    # TODO refactor using ElementTree
-    xml.dom.ext.PrettyPrint(scanLog, stream=f)
-    f.close()
-    return
 
 #-------------------------------------------------
 
 xmlFile = 'scanlog.xml'
 
 if __name__ == "__main__":
-     if len(sys.argv) != 5:
-     	 usage()
-     	 sys.exit()
+    if len(sys.argv) != 5:
+        usage()
+        sys.exit()
 
-     mode = sys.argv[1]
-     number = sys.argv[2]
-     datafile = sys.argv[3]
-     title = sys.argv[4]
+    mode = sys.argv[1]
+    number = sys.argv[2]
+    datafile = sys.argv[3]
+    title = sys.argv[4]
 
-     if (mode == 'started'):
-     	 startScanEntry(xmlFile, number, datafile, title)
-     elif (mode == 'ended'):
-     	 endScanEntry(xmlFile, number, datafile)
-     else:
-     	 usage()
-     	 sys.exit()
+    if (mode == 'started'):
+        startScanEntry(xmlFile, number, datafile, title)
+    elif (mode == 'ended'):
+        endScanEntry(xmlFile, number, datafile)
+    else:
+        usage()
+        sys.exit()
--- USAXS/scanLog/xmlSupport.py
+++ USAXS/scanLog/xmlSupport.py
@@ -14,17 +14,21 @@
 
 import os.path
 import sys
-from xml.dom import minidom
+import time
 from xml.etree import ElementTree
+# SEE https://subversion.xor.aps.anl.gov/trac/bcdaext/browser/pvrrd/xmlSupport.py
+# for pretty XML format
+from xml.dom import minidom
 
 #**************************************************************************
 
 def readPvlistXML(xmlFile):
+    '''get the list of PVs to monitor from the XML file'''
     doc = ElementTree.parse(xmlFile)
     db = []
     for element in doc.findall('EPICS_PV'):
-    	arr = {}
-    	arr['desc'] = element.text.strip()
+        arr = {}
+        arr['desc'] = element.text.strip()
         for attribute in element.attrib.keys():
             arr[attribute] = element.attrib[attribute].strip()
         db.append(arr)
@@ -33,30 +37,132 @@ def readPvlistXML(xmlFile):
 #**************************************************************************
 
 def readConfigurationXML(baseDir):
+    '''locate the PV configuration from XML into memory'''
     config = {}
-    pvListFile = os.path.join(baseDir,'pvlist.xml')
+    pvListFile = os.path.join(baseDir, 'pvlist.xml')
     config['pvList'] = readPvlistXML(pvListFile)
     return(config)
 
 #**************************************************************************
 
-def __parseElement__(element):
-    print(element.name)
-    for attribute in element.xpathEval('@*'):
-    	print("  @ " + attribute.name)
-    for child in element.xpathEval('*'):
-        __parseElement__(child)
+def openScanLogFile(xmlFile):
+    '''open the XML file with ElemenetTree
+        return the doc (doc.getroot() to get the root node)'''
+    return ElementTree.parse(xmlFile)
 
 #**************************************************************************
 
+def locateScanID(doc, id):
+    '''find the XML scan entry with matching id attribute
+        return XML node or None if not found
+    '''
+    result = None
+    #query = "scan/[@id='%s']" % id
+    for node in doc.findall("scan"):
+        if node.get("id") == id:
+            result = node
+            break
+    return result
+
+#**************************************************************************
+
+def flagRunawayScansAsUnknown(doc, id):
+    '''sometimes, a scan ends without this program finding out'''
+    for node in doc.findall("scan"):            # look for any scan ...
+        if node.get("state") == "scanning":     # with state="scanning" ...
+            if node.get("id") != id:            # but not the newest node ...
+                node.set("state", "unknown")    # THIS node is not scanning anymore
+
+#**************************************************************************
+
+def readFileAsLines(filename):
+    '''open a file and read all of it into 
+        memory as a list separated by line breaks'''
+    # FIXME needs error handling
+    f = open(filename, 'r')
+    buf = f.read()
+    f.close()
+    return buf.split("\n")
+
+#**************************************************************************
+
+def writeLinesInFile(filename, lines):
+    '''write a list of lines to a file'''
+    # FIXME needs error handling
+    f = open(filename, 'w')
+    f.write("\n".join(lines))
+    f.close()
+    return
+
+#**************************************************************************
+
+def prettyXml(element):
+    '''indent written XML nicely'''
+    # FIXME poor introduction of whitespace - DO NOT USE YET
+    txt = ElementTree.tostring(element)
+    return minidom.parseString(txt).toprettyxml()
+
+#**************************************************************************
+
+def writeScanLogFile(xmlFile, doc):
+    '''write the XML doc to a file'''
+    doc.write(xmlFile, encoding="UTF-8")
+    # splice in the reference to the XSLT at the top of the file (on line 2)
+    xsltRef = '<?xml-stylesheet type="text/xsl" href="scanlog.xsl" ?>'
+    lines = readFileAsLines(xmlFile)
+    lines.insert(1, xsltRef)
+    writeLinesInFile(xmlFile, lines)
+    return
+
+#**************************************************************************
+
+def appendTextNode(doc, parent, tag, value):
+    '''append a text node to the XML document'''
+    elem = ElementTree.Element(tag)
+    elem.text = value
+    parent.append(elem)
+
+#**************************************************************************
+
+def appendDateTimeNode(doc, parent, tag):
+    '''append a date/time node to the XML document'''
+    elem = ElementTree.Element(tag)
+    elem.set('date', xmlDate())
+    elem.set('time', xmlTime())
+    parent.append(elem)
+
+#**************************************************************************
+
+def xmlDate():
+    '''current date, for use in XML file (ISO8601)'''
+    str = time.strftime('%Y-%m-%d')    # XML date format
+    return str
+
+#**************************************************************************
+
+def xmlTime():
+    '''current time, for use in XML file (ISO8601)'''
+    str = time.strftime('%H:%M:%S')    # XML time format
+    return str
+
+#**************************************************************************
+
+
 if __name__ == "__main__":
-   if (len(sys.argv) == 2):
-   	   pwd = sys.argv[1]
-   else:
-   	   pwd = '.'
-   config = readConfigurationXML(pwd)
-   print "PVs named in configuration:"
-   import pprint
-   pprint.pprint(config)
-   #for pv in config['pvList']:
-   #    print pv['pv']
+    if (len(sys.argv) == 2):
+        pwd = sys.argv[1]
+    else:
+        pwd = '.'
+    config = readConfigurationXML(pwd)
+    #print "PVs named in configuration:"
+    #import pprint
+    #pprint.pprint(config)
+
+    doc = openScanLogFile('scanlog.xml')
+    scan = locateScanID(doc, '43:/share1/USAXS_data/2010-03/03_24_setup.dat')
+    print scan
+    root = doc.getroot()
+    appendTextNode(doc, root, "bewilderment", "is a state of mind")
+    appendDateTimeNode(doc, root, "timestamp")
+    #print prettyXml(root)
+    writeScanLogFile('test.xml', doc)
