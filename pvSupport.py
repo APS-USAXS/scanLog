f91b1bd27b12a3e0d0897c0849f83bd26b82ecbc
Pete R Jemian <prjemian@users.noreply.github.com>
2010-09-10 06:01:21 +0000

    refactor XML support to use ElementTree (lost use of XPath) BUT now ignores name of root element - Is this GOOD?

--- pvSupport.py
+++ pvSupport.py
@@ -49,37 +49,37 @@ def watchThis(pv):
     ch = CaChannel()
     try:
         ch.searchw(pv)
-	user_args = (ch)
-	ch.add_masked_array_event(ca.DBR_STRING, 1, ca.DBE_VALUE, demoEpicsCallback, user_args)
+        user_args = (ch)
+        ch.add_masked_array_event(ca.DBR_STRING, 1, ca.DBE_VALUE, demoEpicsCallback, user_args)
     except:
-        message = time.ctime() 
+        message = time.ctime()
         message += " could not find " + pv
         print message
         sys.stdout.flush()
-	ch = ''
+        ch = ''
     return (ch)
 
 def report():
     for pv in pvList:
         value = db[pv+',value']
-	last_update = db[pv+',time']
-	print pv, '\t', value, '\t', last_update
+        last_update = db[pv+',time']
+        print pv, '\t', value, '\t', last_update
 
 def update(lastScanningState):
     scanningState = db[pvTag['scanning']+',value']
     if (scanningState != lastScanningState):
         # report()
-	number = db[pvTag['number']+',value']
-	directory = db[pvTag['directory']+',value']
-	fileName = db[pvTag['file']+',value']
-	title = db[pvTag['title']+',value']
-	datafile = os.path.join(directory, fileName)
-	if scanningState == 'ON':
-	    scanlog.startScanEntry(cfg['scanLog'], number, datafile, title)
-	elif scanningState == 'OFF':
-	    scanlog.endScanEntry(cfg['scanLog'], number, datafile)
-	else:
-	    print "this should not happen, state = ", scanningState
+        number = db[pvTag['number']+',value']
+        directory = db[pvTag['directory']+',value']
+        fileName = db[pvTag['file']+',value']
+        title = db[pvTag['title']+',value']
+        datafile = os.path.join(directory, fileName)
+        if scanningState == 'ON':
+            scanlog.startScanEntry(cfg['scanLog'], number, datafile, title)
+        elif scanningState == 'OFF':
+            scanlog.endScanEntry(cfg['scanLog'], number, datafile)
+        else:
+            print "this should not happen, state = ", scanningState
     return scanningState
 
 #-------------------------------------------------------------
@@ -101,7 +101,7 @@ if __name__ == "__main__":
 
     for pvEntry in cfg['pvList']:
         setup(pvEntry)
-	pvTag[pvEntry['tag']] = pvEntry['pv']
+        pvTag[pvEntry['tag']] = pvEntry['pv']
 
     lastValue = db[pvTag['scanning']+',value']
 
@@ -109,17 +109,17 @@ if __name__ == "__main__":
     print "# ", time.ctime(), " Started"
     print "#------------------------------"
     while 1:
-    	pv = pvList[0]
-    	ch = db[pv + ',ch']
-    	ch.pend_event()
-    	# all monitors will be received even if just 
-	# one PV's pend_event is called
-	try:
-    		lastValue = update(lastValue)
-	except StandardError, error_message:
-		print "#------------------------------"
-		print "# ", time.ctime(), " Python error report:"
-		print error_message
-		print "#------------------------------"
-    	#report()
-    	time.sleep(1)
+        pv = pvList[0]
+        ch = db[pv + ',ch']
+        ch.pend_event()
+        # all monitors will be received even if just
+        # one PV's pend_event is called
+        try:
+            lastValue = update(lastValue)
+        except StandardError, error_message:
+            print "#------------------------------"
+            print "# ", time.ctime(), " Python error report:"
+            print error_message
+            print "#------------------------------"
+        #report()
+        time.sleep(1)
