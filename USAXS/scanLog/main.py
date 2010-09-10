35fad594181dc9f803fbea7d8b5491928ab13786
jemian <jemian@3cb32c87-4c34-0410-8c40-b60f3c13e47e>
2010-09-10 21:05:05 +0000

    trivial code that identifies which module is the main one

---
+++ USAXS/scanLog/main.py
@@ -0,0 +1,23 @@
+#!/bin/env python
+
+'''
+log EPICS data into RRD files
+
+########### SVN repository information ###################
+# $Date$
+# $Author$
+# $Revision$
+# $HeadURL$
+# $Id$
+########### SVN repository information ###################
+'''
+
+#-------------------------------------------------------------
+
+import pvSupport
+
+#-------------------------------------------------------------
+
+if __name__ == "__main__":
+    ''' show which module is the main code to run '''
+    pvSupport.main()
