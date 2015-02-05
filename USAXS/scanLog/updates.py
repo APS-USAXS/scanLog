d96671a3a75b327e660c53ec0bb7f2e46e6f1852
jemian <jemian@3cb32c87-4c34-0410-8c40-b60f3c13e47e>
2015-02-05 22:32:47 +0000

    style

--- USAXS/scanLog/updates.py
+++ USAXS/scanLog/updates.py
@@ -20,9 +20,11 @@ def usage():
     ''' in case we forget '''
     print 'usage:  updates.py started|ended number fileName "the title" specScanMacro'
 
+
 def buildID(number, fileName):
     ''' return the unique scan index ID based on scan number and file name '''
     return "%s:%s" % (number, fileName)
+
 
 def startScanEntry(scanLogFile, number, fileName, title, macro):
     ''' Start a new scan in the XML file '''
@@ -37,25 +39,25 @@ def startScanEntry(scanLogFile, number, fileName, title, macro):
     if scanNode != None:
         print "ERROR: One or more scans matches in the log file."
         return
-    #---
+
     root = doc.getroot()
     scanEntry = ElementTree.Element('scan')
     scanEntry.set('number', number)
     scanEntry.set('state', 'scanning')
     scanEntry.set('id', scanID)
     scanEntry.set('type', macro)
+
     # put this scan at the end of the list
     root.append(scanEntry)
-    #---
+
     xmlSupport.appendTextNode(doc, scanEntry, 'title', title)
     xmlSupport.appendTextNode(doc, scanEntry, 'file', fileName)
     xmlSupport.appendDateTimeNode(doc, scanEntry, 'started')
-    #---
+
     xmlSupport.flagRunawayScansAsUnknown(doc, scanID)
-    #---
+
     xmlSupport.writeXmlDocToFile(scanLogFile, doc)
-    #---
-    # TODO set demo=False   With below, fixes Trac ticket #9
+
     wwwServerTransfers.scpToWebServer(scanLogFile, 
                   os.path.split(scanLogFile)[-1], demo = False)
 
@@ -81,19 +83,15 @@ def endScanEntry(scanLogFile, number, fileName):
 
 #-------------------------------------------------
 
-# TODO Trac ticket #8: Can we get this from the pvlist.xml file instead?
-# It is already read out by xmlSupport
-
-# TODO Trac ticket #7: directory handling in this support is sloopppy.  Must be cleaned up.
-
 xmlFile = 'scanlog.xml'
 
 if __name__ == "__main__":
     if len(sys.argv) != 6:
+        # TODO: use argparse package
         usage()
         sys.exit()
 
-    (scriptname, mode, number, datafile, title, macro) = sys.argv
+    scriptname, mode, number, datafile, title, macro = sys.argv
     
     # This main() is test code.  The standard support starts with pvSupport.py 
 
