c382940d3af58bbabac222c5dc3f79f25943c885
Pete R Jemian <prjemian@users.noreply.github.com>
2015-02-06 15:24:23 +0000

    define rules for how a scan might be plottable

--- plots.py
+++ plots.py
@@ -13,12 +13,12 @@ SCANLOG = '/share1/local_livedata/scanlog.xml'
 
 class Scan(object):
     
-    def __init__(self, title, data_file, scan_type, scan_number, id):
+    def __init__(self, title, data_file, scan_type, scan_number, idstr):
         self.title = title
         self.scan_type = scan_type
         self.data_file = data_file
         self.scan_number = int(scan_number)
-        self.id = id
+        self.identification = idstr
         # TODO: make a safe ID, suitable for use as an HDF5 label (<63 char, none odd)
         self.Q = None
         self.I = None
@@ -43,12 +43,26 @@ class Scan(object):
             pass
 
 
+def ok_to_plot(scan):
+    ok = False
+    # TODO: test if this scan is already in the cache of known scans with data
+    if scan.attrib['type'] in ('uascan', 'sbuascan'):
+        if scan.attrib['state'] in ('scanning', 'complete'):
+            if os.path.exists(scan.find('file').text.strip()):
+                ok = True
+    elif scan.attrib['type'] in ('FlyScan', ):
+        if scan.attrib['state'] in ('complete', ):
+            # TODO: test that HDF5 data file exists
+            ok = True
+    return ok
+
+
 def last_n_scans(xml_log_file, number_scans):
     xml_doc = xmlSupport.openScanLogFile(SCANLOG)
 
     # make sure only state="complete" scans are selected
     # TODO: make sure data file exists
-    scans = [scan for scan in xml_doc.findall('scan') if scan.attrib['state'] == 'complete']
+    scans = [scan for scan in xml_doc.findall('scan') if ok_to_plot(scan)]
 
     return scans[-number_scans:]
 
@@ -58,10 +72,10 @@ def main():
                  scan.find('file').text.strip(),
                  scan.attrib['type'],
                  scan.attrib['number'],
-                 scan.attrib['id'],
+                 scan.attrib['identification'],
              ) for scan in last_n_scans(SCANLOG, 5)]
     print '\n'.join(map(str, scans))
-    print '\n'.join([_.id for _ in scans])
+    print '\n'.join([_.identification for _ in scans])
     for scan in scans:
         scan.getData()
 
